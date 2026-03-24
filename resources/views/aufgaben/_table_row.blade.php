9e38eb43db1affd755e1c4b68d2d859a603975b7
IT Cockpit <itcockpit@kreis-fs.de>
2026-03-24 11:07:31 +0100

    fix: Aufgaben-Icon – explizite SVG-Größe statt Tailwind-Klassen

--- resources/views/aufgaben/_table_row.blade.php
+++ resources/views/aufgaben/_table_row.blade.php
@@ -67,7 +67,7 @@
                             {{ $aufgabe->name }}
                         </span>
                         @if($aufgabe->beschreibung)
-                            <svg title="Hat Beschreibung" class="w-3.5 h-3.5 text-indigo-400 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <svg title="Hat Beschreibung" width="14" height="14" style="flex-shrink:0;color:#818cf8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/>
                             </svg>
                         @endif
